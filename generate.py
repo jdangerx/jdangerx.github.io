656c76214bb3bf7c6a03c34c85b39367a7681569
John Xia <john.danger.xia@gmail.com>
2018-10-19 17:48:59 -0400

    Autogenerate index

--- generate.py
+++ generate.py
@@ -1,6 +1,7 @@
 #! /usr/bin/env python
 
 import argparse
+from datetime import datetime
 import os
 import re
 import sys
@@ -11,38 +12,75 @@ def read_source(filepath):
     return source
 
 
-def render(source_fp, template_fp):
+def get_title(md):
+    match = re.search(r"#+ (.+)\n", md)
+    if match:
+        return match.groups()[0].strip()
+    return "DAZ.ZONE"
+
+
+def get_info(source_fp):
     with open(source_fp) as f:
         source = f.read()
-    with open(template_fp) as f:
-        template = f.read()
-    match = re.search(r"#+ (.+)\n", source)
-    if match:
-        title = match.groups()[0].strip()
-    else:
-        title = "DAZ.ZONE"
+    title = get_title(source)
+    mtime = os.path.getmtime(source_fp)
+    basename, _ext = os.path.splitext(os.path.basename(source_fp))
+    return {
+        "title": title,
+        "mtime": datetime.fromtimestamp(mtime),
+        "basename": basename,
+        "md": source
+    }
+
+
+def render(source, template):
+    title = get_title(source)
     render_params = {"title": title, "md_src": source}
     html = template.format(**render_params)
     return html
 
 
-def regenerate_all(src_dir="src", out_dir="blog", template="template.html"):
-    src_dir = "src"
-    out_dir = "blog"
+def make_index(infos, template):
+    post_list = [
+         "{mtime:%B %-d, %Y} - [{title}](/blog/{basename}.html)"
+        .format(**info) for info in infos
+    ]
+
+    index_md = "# DAZ.ZONE ARCHIVE\n\n{}\n".format("\n".join(post_list))
+    return render(index_md, template)
+
+
+def make_feed(infos):
+    return "ah"
+
+
+def regenerate_all(src_dir="src/posts", out_dir="blog", template_fp="template.html"):
     if not os.path.isdir(src_dir):
         msg = "{} is not a directory!".format(os.path.abspath(src_dir))
         raise RuntimeError(msg)
+
+    with open(template_fp) as f:
+        template = f.read()
 
     is_md = lambda f: os.path.isfile(os.path.abspath(f)) and os.path.splitext(f)[1] == ".md"
 
     dirname, _subdirs, files = os.walk(src_dir).next()  # no subdirs
     has_dir = [os.path.join(dirname, f) for f in files]
     srcs = [f for f in has_dir if is_md(f)]
+    infos = sorted([get_info(s) for s in srcs], key=lambda i: i["mtime"])
 
-    for src in srcs:
-        basename, _ext = os.path.splitext(os.path.basename(src))
-        out_file = os.path.join(out_dir, "{}.html".format(basename))
-        html = render(src, template)
+    index_html = make_index(infos, template)
+    with open(os.path.join(out_dir, "index.html"), "w") as f:
+        f.write(index_html)
+
+    atom_xml = make_feed(infos)
+    with open(os.path.join(out_dir, "atom-feed.xml"), "w") as f:
+        f.write(atom_xml)
+
+    for info in infos:
+        html = render(info["md"], template)
+
+        out_file = os.path.join(out_dir, "{}.html".format(info["basename"]))
         print("Writing to {}...".format(out_file))
         with open(out_file, "w") as f:
             f.write(html)
--- src/index.md
+++
@@ -1,3 +0,0 @@
-# DAZ.ZONE ARCHIVE
-
-Oct 19, 2018 [Writing a small browser extension](/blog/busybody.html)
