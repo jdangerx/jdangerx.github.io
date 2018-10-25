778e2e66f4c7406877223bfd7132dde08d04c3da
John Xia <john.danger.xia@gmail.com>
2018-10-25 16:08:21 -0400

    remove console.log, i'm a noob

--- static/blog.js
+++ static/blog.js
@@ -4,7 +4,6 @@ function render() {
   const content = document.getElementById("content");
   const mdSrc = document.getElementById("md-src");
 
-  console.log(mdSrc);
   const mdRendered = md.render(mdSrc.innerHTML);
   content.innerHTML = mdRendered;
   content.style = "";
